3e702f859e92b3d2204523be63e1f457aa57ed70
shahajan4617 <141109594+shahajan4617@users.noreply.github.com>
2023-08-10 13:37:58 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-app', //template name
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
