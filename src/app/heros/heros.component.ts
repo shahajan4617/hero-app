a5edb3fd3c58f0b0de44c523e35eb601e8bc4a95
shahajan4617 <141109594+shahajan4617@users.noreply.github.com>
2023-08-11 11:16:34 +0530

    2 files modified, 1 added

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -2,23 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { Heros } from '../mock-heros';
 
 @Component({
   standalone: true,
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css'],
-  imports: [CommonModule,FormsModule]
+  imports: [CommonModule, FormsModule],
 })
 export class HerosComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: 'HULK'
+    name: 'HULK',
   };
+  heros = Heros;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/mock-heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+// import { Heros } from '../mock-heros';
+
+export const Heros: Hero[] = [
+  { id: 12, name: 'MSD' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Superman' },
+  { id: 15, name: 'X-man' },
+  { id: 16, name: 'ABD' },
+  { id: 17, name: 'Deadpool' },
+  { id: 18, name: 'rashid' },
+  { id: 19, name: 'virat' },
+  { id: 20, name: 'rohit' },
+];
